2a5291b538aac676dc3657698f7cef8299bdcdda
matheuscosta15 <111282524+matheuscosta15@users.noreply.github.com>
2023-10-03 09:35:15 -0300

    Add files via upload

--- main (3).js
+++ main (3).js
@@ -1,4 +1,34 @@
-function tocaSomPom () { 
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) { 
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('#som_tecla_pom')
+
+
+const ListadeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+//enquanto
+white (contador < ListadeTeclas.length) {
+
+    const tecla = ListadeTeclas[contador]
+
+    const instrumento = ListadeTeclas[contador].classList[1];
+
+     
+
+     //template string
+
+     console.log(idAudio)
+
+     const idAudio = '#som_${instrumento}';
+
+
+tecla.onclick = function () {
+    tocaSom( idAudio);
+}
+
+contador = contador + 1;
+
+console.log(contador);
+
+}
